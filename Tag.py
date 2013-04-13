6618fb7d716f3ca3dffcff66c67d15e172122812
Jay <jayzawrotny@gmail.com>
2013-04-13 14:36:21 -0400

    Commented out unparsable script and style settings.

--- Tag.py
+++ Tag.py
@@ -100,29 +100,29 @@ class Tag():
 				content = content.replace(comment, (len(comment))*'.')
 
 		# script
-		unparseable = content.split('<script')
-		content = unparseable.pop(0)
-		l = len(unparseable)
-		i = 0
-		while i < l:
-			tmp = unparseable[i].split('</script>')
-			content += '.......'
-			content += len(tmp.pop(0))*'.'
-			content += '.........'
-			content += ".........".join(tmp)
-			i += 1
+		# unparseable = content.split('<script')
+		# content = unparseable.pop(0)
+		# l = len(unparseable)
+		# i = 0
+		# while i < l:
+		# 	tmp = unparseable[i].split('</script>')
+		# 	content += '.......'
+		# 	content += len(tmp.pop(0))*'.'
+		# 	content += '.........'
+		# 	content += ".........".join(tmp)
+		# 	i += 1
 
 		# style
-		unparseable = content.split('<style')
-		content = unparseable.pop(0)
-		l = len(unparseable)
-		i = 0
-		while i < l:
-			tmp = unparseable[i].split('</style>')
-			content += '......'
-			content += len(tmp.pop(0))*'.'
-			content += '........'
-			content += "........".join(tmp)
-			i += 1
+		# unparseable = content.split('<style')
+		# content = unparseable.pop(0)
+		# l = len(unparseable)
+		# i = 0
+		# while i < l:
+		# 	tmp = unparseable[i].split('</style>')
+		# 	content += '......'
+		# 	content += len(tmp.pop(0))*'.'
+		# 	content += '........'
+		# 	content += "........".join(tmp)
+		# 	i += 1
 
 		return content
